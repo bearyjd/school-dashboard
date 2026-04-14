5744613063313b897d9af3509f3177a8ae13798b
tower <jd@beary.us>
2026-04-13 22:47:37 -0400

    fix: add SpeechRecognitionEvent declaration to vite-env.d.ts

--- web/spa/src/vite-env.d.ts
+++ web/spa/src/vite-env.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="vite/client" />
 
+interface SpeechRecognitionEvent extends Event {
+  results: SpeechRecognitionResultList
+}
+
 interface SpeechRecognition extends EventTarget {
   onresult: ((e: SpeechRecognitionEvent) => void) | null
   start(): void
